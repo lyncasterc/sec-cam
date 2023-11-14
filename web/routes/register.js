521e935cfbc2df68cf392d053b8a4ee1dd2026d8
stivencabrera3 <stiven.cabrera3@gmail.com>
2023-11-13 23:19:05 -0500

    Add camera-setup route
    Also a bunch of rewrites to other routes to handle different req.session states
    
    also adds the /register/cancel route

--- web/routes/register.js
+++ web/routes/register.js
@@ -1,15 +1,82 @@
 import express from 'express';
 import { body, validationResult } from 'express-validator';
+import { v4 } from 'uuid';
 import { User, Camera } from '../mongo/index.js';
+import userService from '../services/user-service.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('register');
+  // If user is already logged in, redirect to home page.
+  if (req.session.isLoggedIn) {
+    return res.redirect('/');
+  }
+
+  // if user is already in registration process, redirect to camera setup page.
+  if (req.session.inRegistrationProcess) {
+    return res.redirect('/register/camera-setup');
+  }
+
+  return res.render('register');
 });
 
-router.get('/camera-setup', (req, res) => {
-  res.render('camera-setup');
+router.get('/camera-setup', async (req, res) => {
+  // If user is already logged in, redirect to home page.
+  if (req.session.isLoggedIn) {
+    return res.redirect('/');
+  }
+
+  // If user is not in registration process, redirect to login page.
+  if (!req.session.inRegistrationProcess) {
+    return res.redirect('/login');
+  }
+
+  // If registration session has expired, delete user and camera from database.
+  if (Date.now() >= req.session.registrationEnd) {
+    return res.redirect('/register/session-expired');
+  }
+
+  // generate and update token field of User
+  const token = v4();
+  const { username, cameraId } = req.session;
+  await User.updateOne({ username }, { messageToken: token });
+
+  return res.render('camera-setup', {
+    cameraId,
+    username,
+    token,
+    remainingTime: req.session.registrationEnd - Date.now(),
+  });
+});
+
+router.get('/session-expired', async (req, res) => {
+  // If user is already logged in, redirect to home page.
+  if (req.session.isLoggedIn) {
+    return res.redirect('/');
+  }
+
+  // If user is not in registration process, redirect to login page.
+  if (!req.session.inRegistrationProcess) {
+    return res.redirect('/login');
+  }
+
+  // If registration session has expired, delete user and camera from database.
+  if (Date.now() >= req.session.registrationEnd) {
+    try {
+      const { username } = req.session;
+
+      await userService.deleteUserByUsername(username);
+
+      req.session.destroy();
+
+      return res.render('/register', { error: 'Registration session expired. Please try again.' });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send({ error: 'Something went wrong. Please try again.' });
+    }
+  }
+
+  return res.redirect('/register/camera-setup');
 });
 
 router.post('/', [
@@ -73,9 +140,14 @@ router.post('/', [
     // Mark session as in registration process.
     // User is NOT logged in yet until they connect their camera.
     // Session will expire after 15 minutes.
+
     req.session.inRegistrationProcess = true;
-    req.session.cookie.maxAge = 15 * 60 * 1000;
+    req.session.isLoggedIn = false;
     req.session.registrationEnd = Date.now() + 15 * 60 * 1000;
+    req.session.cameraId = cameraId;
+    req.session.username = username;
+
+    req.session.save();
     return res.redirect('/register/camera-setup');
   } catch (error) {
     console.error(error);
@@ -86,5 +158,24 @@ router.post('/', [
 // router.post('/confirm', (req, res) => {
 
 // });
+
+router.delete('/cancel', async (req, res) => {
+  if (req.session.inRegistrationProcess) {
+    try {
+      const { username } = req.session;
+
+      await userService.deleteUserByUsername(username);
+
+      req.session.destroy();
+
+      return res.redirect('/register');
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send({ error: 'Something went wrong. Please try again.' });
+    }
+  }
+
+  return res.status(400);
+});
 
 export default router;
