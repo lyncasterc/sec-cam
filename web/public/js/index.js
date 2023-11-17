d3bec7976341e725851fc59a0393fe4d41420acb
stivencabrera3 <stiven.cabrera3@gmail.com>
2023-11-17 16:58:10 -0500

    Create home page route and view

---
+++ web/public/js/index.js
@@ -0,0 +1,170 @@
+/* eslint-disable no-undef */
+
+const ws = new WebSocket(`wss://${window.location.host}`);
+const viewBtns = document.querySelectorAll('.view-btn');
+const remoteVideo = document.querySelector('.remote-video');
+const camerasContainer = document.querySelector('.cameras-container');
+const config = {
+  iceServers: [...turnIceServers, { urls: 'stun:stun.l.google.com:19302' }],
+};
+const loader = document.querySelector('.loader');
+const logoutBtn = document.querySelector('.logout-btn');
+const pc = new RTCPeerConnection(config);
+let selectedCameraId;
+
+/**
+ * Sets the status of the cameras by making a POST request to the '/api/camera-status' endpoint
+ * and updating the corresponding HTML elements with the status information.
+ * @param {Array} cameras - An array of camera objects.
+ * @returns {Promise<void>} - A Promise that resolves
+ * when the status of all cameras has been updated.
+ */
+async function setCamerasStatuses(cameras) {
+  try {
+    const response = await fetch('/api/camera-status', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ cameras }),
+    });
+
+    const camerasStatuses = await response.json();
+
+    camerasStatuses.forEach((cameraStatus) => {
+      const cameraStatusElement = document.querySelector(`.camera-container[data-cameraId="${cameraStatus.id}"] .camera-status`);
+
+      if (cameraStatus.isConnected) {
+        cameraStatusElement.classList.add('connected');
+      } else {
+        cameraStatusElement.classList.remove('connected');
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// send a register message to the signal channel when the connection is opened
+ws.addEventListener('open', () => {
+  ws.send(JSON.stringify({
+    type: 'register',
+    sender: user.username,
+    target: 'server',
+    clientType: 'user',
+    data: {
+      token: user.token,
+      inRegistrationProcess: false,
+      registeredCameras: cameras.map((camera) => camera.id),
+    },
+  }));
+});
+
+// setting the remoteVideo srcObject when a remote track is received
+pc.ontrack = ({ track, streams }) => {
+  console.log('Got remote track and streams:');
+
+  if (remoteVideo.srcObject) {
+    return;
+  }
+
+  [remoteVideo.srcObject] = streams;
+
+  loader.classList.add('hide');
+  remoteVideo.parentElement.classList.remove('hide');
+  camerasContainer.classList.add('hide');
+};
+
+// sending ice candidates to the camera through the signal channel
+pc.onicecandidate = ({ candidate }) => {
+  if (candidate) {
+    ws.send(JSON.stringify({
+      type: 'ice-candidate',
+      sender: user.username,
+      target: selectedCameraId,
+      clientType: 'user',
+      data: candidate,
+    }));
+  }
+};
+
+// sending the offer to the camera through the signal channel,
+// starting the WebRTC negotiation process
+viewBtns.forEach((viewBtn) => {
+  viewBtn.addEventListener('click', async () => {
+    selectedCameraId = viewBtn.parentElement.dataset.cameraid;
+    loader.classList.remove('hide');
+    viewBtn.disabled = true;
+
+    try {
+      const offer = await pc.createOffer({
+        offerToReceiveAudio: false,
+        offerToReceiveVideo: true,
+      });
+
+      await pc.setLocalDescription(offer);
+
+      console.log('Sending offer to camera: ', selectedCameraId);
+
+      ws.send(JSON.stringify({
+        type: 'offer',
+        sender: user.username,
+        target: selectedCameraId,
+        clientType: 'user',
+        data: pc.localDescription,
+      }));
+    } catch (error) {
+      console.error('Error creating offer: ', error);
+    }
+  });
+});
+
+logoutBtn.addEventListener('click', async () => {
+  try {
+    const response = await fetch('/api/logout', {
+      method: 'POST',
+      credentials: 'include',
+    });
+
+    if (response.ok && response.redirected) {
+      window.location.href = response.url;
+    } else {
+      console.error('Failed to logout');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// handling answer or ice candidates from camera
+
+ws.addEventListener('message', async (event) => {
+  console.log('Got message from signaling server');
+
+  const parsedData = JSON.parse(event.data);
+  const { type } = parsedData;
+
+  try {
+    if (type === 'answer') {
+      const { data } = parsedData;
+      const answer = new RTCSessionDescription(data);
+      await pc.setRemoteDescription(answer);
+    } else if (type === 'ice-candidate') {
+      const { data } = parsedData;
+      const candidate = new RTCIceCandidate(data);
+      await pc.addIceCandidate(candidate);
+    } else {
+      console.warn('Unrecognized message type:', type);
+    }
+  } catch (err) {
+    console.error('Failed to handle message', err);
+  }
+});
+
+setCamerasStatuses(cameras);
+
+// setting the status of the cameras every 10 seconds
+setInterval(async () => {
+  await setCamerasStatuses(cameras);
+}, 10000);
--- web/routes/index.js
+++ web/routes/index.js
@@ -1,25 +1,71 @@
+/* eslint-disable no-await-in-loop */
 import express from 'express';
+import { v4 } from 'uuid';
 import { Camera } from '../mongo/index.js';
+import { userService } from '../services/index.js';
+import config from '../utils/config.js';
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   if (req.session.user && req.session.user.isLoggedIn) {
-    const cameras = await Camera.find({ owner: req.session.user.id });
+    try {
+      const cameras = await Camera.find({ owner: req.session.user.id });
 
-    if (cameras && cameras.length > 0) {
-      const camerasInfo = cameras.map((camera) => ({
-        id: camera.cameraId,
-        name: camera.name,
-        owner: camera.owner,
-      }));
+      if (cameras && cameras.length > 0) {
+        const camerasInfo = cameras.map((camera) => ({
+          id: camera.cameraId,
+          name: camera.name,
+          owner: camera.owner,
+        }));
 
-      return res.render('index', { user: req.session.user, cameras: camerasInfo });
+        let iceServers = null;
+        let attempts = 0;
+
+        // attempt to fetch TURN server credentials 3 times
+        while (attempts < 3) {
+          const response = await fetch(`https://sec-cam.metered.live/api/v1/turn/credentials?apiKey=${config.METERED_API_KEY}`);
+
+          if (response.ok) {
+            iceServers = await response.json();
+            break;
+          } else {
+            // eslint-disable-next-line no-plusplus
+            attempts++;
+            console.error(`Attempt ${attempts} to fetch TURN server credentials failed. Retrying...`);
+          }
+        }
+
+        if (!iceServers) {
+          throw new Error('Failed to fetch TURN server credentials after 3 attempts');
+        }
+
+        const token = v4();
+
+        await userService.updateUserByUsername(
+          req.session.user.username,
+          { messageToken: token },
+        );
+
+        return res.render('index', {
+          user: {
+            ...req.session.user,
+            token,
+          },
+          cameras: camerasInfo,
+          iceServers,
+        });
+      }
+
+      return res.status(500).send({
+        error: 'Something went wrong. Please try again later.',
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send({
+        error: 'Something went wrong. Please try again later.',
+      });
     }
-
-    return res.status(500).send({
-      error: 'Something went wrong. Please try again later.',
-    });
   }
 
   if (req.session.inRegistrationProcess) {
